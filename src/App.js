c20b7c3c93b988e154068467775dd2dfa51f1261
Muhammad Iqbal <hellofromiqbal@gmail.com>
2023-12-18 14:43:57 +0700

    edit second paragraph

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ function App() {
           Edit <code>src/App.js</code> and save to reload.
         </p>
         <p>
-          Danza Kuduro
+          Don Omar - Danza Kuduro
         </p>
         <a
           className="App-link"
